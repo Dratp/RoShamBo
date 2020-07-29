1a16c17b0d006bc49e45836af75f7fe2c8d094b3
David Heizer <DavidHeizer@QuickenLoans.com>
2020-07-29 18:01:11 -0400

    This is my initial commit

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome player 1 what is your name: ");
+            string name = Console.ReadLine();
+            Human player1 = new Human(name);
+            int winner = 0;
+            bool validPlayer2 = false;
+            string input = "";
+
+            int randyWin = 0;
+            int randyGames = 0;
+            int brockWin = 0;
+            int brockGames = 0;
+            int humanWin = 0;
+            int humanGames = 0;
+
+            while (!validPlayer2)
+            {
+                Console.WriteLine("Would you like to play against Randy or Brock?: ");
+                input = Console.ReadLine();
+                if (input.ToLower() == "brock" || input.ToLower() == "b")
+                {
+                    Brock player2 = new Brock();
+                    Roshambo p1 = player1.GenerateRoShamBo();
+                    Roshambo p2 = player2.GenerateRoShamBo();
+                    humanGames++;
+                    brockGames++;
+                    winner = player.Compare(p1, p2);
+                    Console.WriteLine(String.Format("Game {0,4}\t{1,10}\t{2,-10}winner = {3,4}", humanGames, p1, p2, winner));
+                    if (winner == 2)
+                    {
+                        brockWin++;
+                    }
+                    else if (winner == 1)
+                    {
+                        humanWin++;
+                    }
+                    validPlayer2 = true;
+                }
+                else if (input.ToLower() == "randy" || input.ToLower() == "r")
+                {
+                    Randy player2 = new Randy();
+                    Roshambo p1 = player1.GenerateRoShamBo();
+                    Roshambo p2 = player2.GenerateRoShamBo();
+                    humanGames++;
+                    randyGames++;
+                    winner = player.Compare(p1, p2);
+                    Console.WriteLine(String.Format("Game {0,4}\t{1,10}\t{2,-10}winner = {3,4}", humanGames, p1, p2, winner));
+                    if (winner == 2)
+                    {
+                        randyWin++;
+                    }
+                    else if (winner == 1)
+                    {
+                        humanWin++;
+                    }
+                    validPlayer2 = true;
+                }
+                else
+                {
+                    Console.WriteLine("That was not valid");
+                }
+            }
+
+
+
+            
+            
+
+        }
+    }
+}
---
+++ player.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RockPaperScissors
+{
+    enum Roshambo
+    {
+        Rock,
+        Paper,
+        Scissors
+    } 
+    abstract class player
+    {
+        public string Name { get; set; }
+        public Roshambo RPS { get; set; }
+
+        public abstract Roshambo GenerateRoShamBo();
+
+        public override string ToString()
+        {
+            return $"{RPS}";
+        }
+
+        public static int Compare(Roshambo p1, Roshambo p2)
+        {
+            int winner = 0;
+            if (p1 == p2)
+            {
+                winner = 0;
+            }
+            else if (p1 == Roshambo.Rock)
+            {
+                if (p2 == Roshambo.Paper)
+                {
+                    winner = 2;
+                }
+                else winner = 1;
+            }
+            else if (p1 == Roshambo.Paper)
+            {
+                if (p2 == Roshambo.Rock)
+                {
+                    winner = 1;
+                }
+                else
+                {
+                    winner = 2;
+                }
+            }
+            else
+            {
+                if (p2 == Roshambo.Rock)
+                {
+                    winner = 2;
+                }
+                else
+                {
+                    winner = 1;
+                }
+            }
+            return winner;
+        }
+
+    }
+
+    class Brock : player
+    {
+        public Brock()
+        {
+            Name = "Brock";
+        }
+        
+        public override Roshambo GenerateRoShamBo()
+        {
+            Roshambo Rock = Roshambo.Rock;
+            return Rock;
+        }
+    }
+
+    class Randy : player
+    {
+        static Random rand = new Random();
+
+        public Randy()
+        {
+            Name = "Randy";
+        }
+
+        public override Roshambo GenerateRoShamBo()
+        {
+            int RPSnum = rand.Next(3);
+            Roshambo randomRPS = (Roshambo)RPSnum;
+
+            return randomRPS;
+        }
+    }
+
+    class Human : player
+    {
+        public Human(string aName)
+        {
+            Name = aName;
+        }
+
+        public override Roshambo GenerateRoShamBo()
+        {
+            Roshambo PRS = Roshambo.Scissors;
+            bool isValid = false;
+            while (!isValid)
+            {
+                Console.Write("Rock, Paper or Scissors: ");
+                string input = Console.ReadLine();                
+                if (input.ToLower() == "rock" || input.ToLower() == "r")
+                {
+                    PRS = Roshambo.Rock;
+                    isValid = true;
+                }
+                else if(input.ToLower() == "paper" || input.ToLower() == "p")
+                {
+                    PRS = Roshambo.Paper;
+                    isValid = true;
+                }
+                else if (input.ToLower() == "scissors" || input.ToLower() == "s")
+                {
+                    PRS = Roshambo.Scissors;
+                    isValid = true;
+                }
+                else
+                {
+                    Console.WriteLine("That was not a valid choice");
+                }
+            }
+            return PRS;
+        }
+
+    }
+
+}
